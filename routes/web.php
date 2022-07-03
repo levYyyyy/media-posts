b5513d786a4a3cc66ef4889aca99db189987ff67
levYyyyy <107996072+levYyyyy@users.noreply.github.com>
2022-07-03 22:36:53 +0300

    Lol

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
-Route::get('/posts'                    [PostController::class, "getAllPosts"]);
+Route::get('/posts', [PostController::class, "getAllPosts"]);
 
 #Delete method
-Route::get('/post/delete/{id}'               [PostController::class, "deletePostById"]);
+Route::get('/post/delete/{id}', [PostController::class, "deletePostById"]);
 
-Route::get('/post/id/{id}'                [PostController::class, "getPostById"]);
+Route::get('/post/id/{id}', [PostController::class, "getPostById"]);
 
-Route::get('/post/create'              [PostController::class, "getCreatePostPage"]);
-Route::post('/post/create'                                     [PostController::class, "createPost"]);
+Route::get('/post/create', [PostController::class, "getCreatePostPage"]);
+Route::post('/post/create', [PostController::class, "createPost"]);
